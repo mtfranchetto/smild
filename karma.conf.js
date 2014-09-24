54f9242fe897ff4f88f4b3b3ebd026f28099e4b5
Mattia Franchetto <mt.franchetto@gmail.com>
2014-09-24 16:48:04 +0200

    Initial import

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+var cwd = process.cwd();
+
+module.exports = function(config) {
+  config.set({
+    basePath: cwd,
+    frameworks: ['jasmine', 'browserify'],
+    files: [
+      'test/*'
+    ],
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    runnerPort: 9100,
+    colors: true,
+    logLevel: config.LOG_DEBUG,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
+    singleRun: false,
+    browserify: {
+      watch: true,
+      debug: true,
+      transform: ['browserify-shim', 'browserify-istanbul']
+    },
+    preprocessors: {
+        'test/*': ['browserify'],
+        'scripts/*': ['coverage']
+    },
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+  });
+};
