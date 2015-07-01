5f692e375aaa0b05b0a77a98b2ead240e56a5b3d
Mattia Franchetto <mt.franchetto@gmail.com>
2015-07-01 11:02:23 +0200

    Add styles task

--- build.js
+++ build.js
@@ -7,15 +7,10 @@ module.exports = function (gulp, options) {
         streamify = require('gulp-streamify'),
         gulpif = require('gulp-if'),
         merge = require('merge-stream'),
-        autoprefixer = require('gulp-autoprefixer'),
-        minify = require('gulp-minify-css'),
-        concat = require('gulp-concat'),
         embedlr = require('gulp-embedlr'),
         rename = require('gulp-rename'),
         changed = require('gulp-changed'),
         minimist = require('minimist'),
-        sass = require('gulp-sass'),
-        sourcemaps = require('gulp-sourcemaps'),
         express = require('express'),
         refresh = require('gulp-livereload'),
         async = require('async'),
@@ -49,21 +44,5 @@ module.exports = function (gulp, options) {
             currentVariant = variant;
             gulp.series(gulp.parallel(['views', 'styles', 'images', 'assets', 'browserify']), 'rev', 'manifest', 'post-build', callback);
         });
-    });
-
-    !options.module && gulp.task('styles', function () {
-        var bootstrapperPath = path.resolve(options.targets, getVariantPart(), 'bootstrapper.scss');
-        if (!fs.existsSync(bootstrapperPath)) {
-            console.warn("Styles bootstrapper not found at path", bootstrapperPath, ", skipping styles build process.");
-        }
-        return gulp.src(bootstrapperPath)
-            .pipe(gulpif(!isRelease(), sourcemaps.init()))
-            .pipe(concat(BUNDLE_FILENAME + '.css'))
-            .pipe(sass({includePaths: ['./']}).on('error', sass.logError))
-            .pipe(autoprefixer({browsers: options.autoprefixerRules}))
-            .pipe(gulpif(!isRelease(), sourcemaps.write()))
-            .pipe(gulpif(isRelease(), minify()))
-            .pipe(gulp.dest(getTemporaryDirectory() + 'css/'))
-            .pipe(gulpif(watching, refresh(lrserver)));
     });
 };
