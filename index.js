98e9a434ca9ff383adb960eab367da3d497f2b30
Mattia Franchetto <mt.franchetto@gmail.com>
2015-07-01 15:15:27 +0200

    Massive bugfixes

--- index.js
+++ index.js
@@ -47,6 +47,22 @@ gulp.on('error', function (event) {
     console.error(msg);
 });
 
+function formatError(e) {
+    if (!e.error) {
+        return e.message;
+    }
+    // PluginError
+    if (typeof e.error.showStack === 'boolean') {
+        return e.error.toString();
+    }
+    // Normal error
+    if (e.error.stack) {
+        return e.error.stack;
+    }
+    // Unknown (string, number, etc.)
+    return new Error(String(e.error)).stack;
+}
+
 module.exports = {
     taskRunner: taskRunner,
     buildHelper: buildHelper
