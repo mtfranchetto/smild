a2ff5cca1056e05a62a07b839ba195537e5f0317
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-14 18:10:21 +0100

    Add scaffolding to mappings

--- lib/tasks/Mappings.ts
+++ lib/tasks/Mappings.ts
@@ -4,24 +4,28 @@ import Test from "./Test";
 import Typescript from "./Typescript";
 import Build from "./Build";
 import WatchBuild from "./WatchBuild";
+import Scaffolding from "./Scaffolding";
 const gulp = require("gulp");
 
 export const frontend = {
     "clean": Clean,
     "build": Build,
     "watch-build": gulp.series(Clean, WatchBuild),
-    "test": Test
+    "test": Test,
+    "new": Scaffolding
 };
 
 export const module = {
     "clean": Clean,
     "build": Typescript,
-    "test": Test
+    "test": Test,
+    "new": Scaffolding
 };
 
 export const node = {
     "clean": Clean,
     "build": Typescript,
     "watch-build": NodeWatchBuild,
-    "test": Test
+    "test": Test,
+    "new": Scaffolding
 };
