11cf91cad48eef33b0744861e3fceea888e6dc27
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-15 16:48:48 +0100

    Fix scaffolding path

--- lib/tasks/Scaffolding.ts
+++ lib/tasks/Scaffolding.ts
@@ -4,7 +4,7 @@ const replace = require('gulp-replace');
 import * as path from "path";
 
 export default function Scaffolding() {
-    let projectPath = path.resolve(__dirname, "../../../scaffolding", helper.getProjectType());
+    let projectPath = path.resolve(__dirname, "../../scaffolding", helper.getProjectType());
     return gulp.src(projectPath + '/**/*')
         .pipe(replace("$$NAME", helper.getCurrentTarget()))
         .pipe(gulp.dest("./"));
