d11c81fcc4fdbb8a90e658149cd5adbc43655c9b
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-16 13:09:27 +0100

    Add coverage

--- lib/tasks/Test.ts
+++ lib/tasks/Test.ts
@@ -1,13 +1,21 @@
 import {buildHelper as helper, taskRunner} from "../Container";
-const mocha = require("gulp-mocha");
 const gulp = require("gulp");
+const run = require("gulp-run");
+import * as path from "path";
 
-export default function Test() {
-    return gulp.src(helper.getSettings().test, {read: false})
-        .pipe(mocha({
-            reporter: 'spec',
-            compilers: {
-                ts: require('ts-node/register')
-            }
-        }));
+export default function Test(cb) {
+    let settings = helper.getSettings(),
+        modulesPath = path.resolve(__dirname, "../../node_modules"),
+        nyc = path.join(modulesPath, '.bin/nyc'),
+        tsNode = path.join(modulesPath, 'ts-node/register'),
+        mocha = path.join(modulesPath, '.bin/_mocha'),
+        nycSettings = "";
+    nycSettings += `--include '${settings.scripts}'`;
+    nycSettings += ` --exclude node_modules --exclude ${settings.distribution}`;
+    nycSettings += " --extension .ts --extension .tsx";
+    nycSettings += " --reporter text-summary --reporter html";
+    nycSettings += " --sourceMap true --instrument true";
+    return run(`${nyc} ${nycSettings} --require ${tsNode} ${mocha} -- '${settings.test}'`)
+        .exec()
+        .on("error", (error) => console.error(error));
 }
