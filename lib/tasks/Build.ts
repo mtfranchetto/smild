30af7505b3cec765e00cee4183503608d4a68294
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-16 15:16:20 +0100

    Remove bluebird in favor of async await

--- lib/tasks/Build.ts
+++ lib/tasks/Build.ts
@@ -1,7 +1,6 @@
 import {buildHelper as helper, taskRunner} from "../Container";
 import {cyan} from "chalk";
 const gulp = require("gulp");
-import * as Promise from "bluebird";
 import Styles from "./Styles";
 import Images from "./Images";
 import Assets from "./Assets";
@@ -10,19 +9,21 @@ import Revision from "./Revision";
 import Manifest from "./Manifest";
 import CopyIndex from "./CopyIndex";
 import {PreBuild, PostBuild} from "./Hooks";
+import {map} from "lodash";
 
 export default function Build() {
     let targets: string[] = [];
-    if (helper.isWatching() || helper.getCurrentTarget() !== 'all') {
+    if (helper.isWatching() || helper.getCurrentTarget() !== "all") {
         targets = [helper.getCurrentTarget()];
     } else {
         targets = helper.getTargets();
     }
-    return Promise.resolve<any>(targets)
-        .mapSeries((target: string) => {
-            helper.setTarget(target);
-            console.log(cyan("Building target", target));
-            return taskRunner.run(gulp.series(PreBuild, gulp.parallel([CopyIndex, Styles, Images, Assets, Browserify]), Revision, Manifest, PostBuild))
-                .then(() => console.log(cyan("Finished target", target)));
-        });
+    return Promise.all(map(targets, async function (target: string) {
+        helper.setTarget(target);
+        console.log(cyan("Building target", target));
+        await taskRunner.run(gulp.series(PreBuild, gulp.parallel([CopyIndex, Styles, Images, Assets, Browserify]), Revision, Manifest, PostBuild));
+        console.log(cyan("Finished target", target));
+    }));
 }
+
+
