4c034db4a26adb17e3c6d8cd595d8fb3d4c1d0ef
Mattia Franchetto <mt.franchetto@gmail.com>
2016-12-19 19:42:47 +0100

    Separate test and coverage

---
+++ lib/tasks/Coverage.ts
@@ -0,0 +1,27 @@
+import {buildHelper as helper, taskRunner} from "../Container";
+const gulp = require("gulp");
+const run = require("gulp-run");
+import * as path from "path";
+
+export default function Coverage() {
+    let settings = helper.getSettings(),
+        modulesPath = path.resolve(__dirname, "../../node_modules"),
+        nyc = path.join(modulesPath, '.bin/nyc'),
+        tsNode = path.join(modulesPath, 'ts-node/register'),
+        mocha = path.join(modulesPath, '.bin/_mocha'),
+        nycSettings = "";
+    nycSettings += `--include '${settings.scripts}'`;
+    nycSettings += ` --exclude node_modules --exclude ${settings.distribution}`;
+    nycSettings += " --extension .ts --extension .tsx";
+    nycSettings += " --reporter text-summary --reporter html";
+    nycSettings += " --sourceMap true --instrument true";
+    let command = new run.Command(`${nyc} ${nycSettings} --require ${tsNode} ${mocha} -- '${settings.test}'`, {
+        silent: true
+    });
+    return new Promise((resolve, reject) => {
+        command.exec(undefined, (error) => {
+            if (error) reject(error);
+            else resolve();
+        });
+    });
+}
--- lib/tasks/Mappings.ts
+++ lib/tasks/Mappings.ts
@@ -5,18 +5,21 @@ import Typescript from "./Typescript";
 import Build from "./Build";
 import WatchBuild from "./WatchBuild";
 import Scaffolding from "./Scaffolding";
+import Coverage from "./Coverage";
 const gulp = require("gulp");
 
 export const frontend = {
     "build": gulp.series(Clean, Build),
     "watch-build": WatchBuild,
     "test": Test,
+    "coverage": Coverage,
     "new": Scaffolding
 };
 
 export const module = {
     "build": Typescript,
     "test": Test,
+    "coverage": Coverage,
     "new": Scaffolding
 };
 
@@ -24,5 +27,6 @@ export const nodejs = {
     "build": Typescript,
     "watch-build": NodeWatchBuild,
     "test": Test,
+    "coverage": Coverage,
     "new": Scaffolding
 };
