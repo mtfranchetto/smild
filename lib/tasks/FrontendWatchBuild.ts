898ea680c674aa4de71619a9a4a81ad436b67fd1
Mario Gualtieri <mario.gualtieri@3punto6.com>
2017-05-29 15:02:03 +0200

    Add global scss watcher

--- lib/tasks/FrontendWatchBuild.ts
+++ lib/tasks/FrontendWatchBuild.ts
@@ -7,12 +7,8 @@ import Clean from "./Clean";
 const gulp = require("gulp");
 
 export default function FrontendWatchBuild() {
-    let settings = helper.getSettings(),
-        target = helper.getCurrentTarget();
     helper.enableWatch();
     taskRunner.run(gulp.series(Clean, Build, Serve)).then(() => {
-        gulp.watch([path.resolve(settings.targets, target, 'bootstrapper.scss'),
-            path.resolve(settings.targets, 'base.scss'),
-            path.resolve(process.cwd(), settings.styles) + '/**/*.scss'], gulp.parallel(Styles));
+        gulp.watch(path.resolve(process.cwd()) + '/**/*.scss',  gulp.parallel(Styles));
     });
 }
